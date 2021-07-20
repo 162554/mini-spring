504dd7c84c57ab48556c183dca7a29dad05414bb
gxhao <you@example.com>
2021-07-21 01:46:19 +0800

    [一些自己的注释]

--- src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
+++ src/test/java/org/springframework/test/ioc/BeanDefinitionAndBeanDefinitionRegistryTest.java
@@ -9,7 +9,12 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
  * @date 2020/11/24
  */
 public class BeanDefinitionAndBeanDefinitionRegistryTest {
-
+	/*
+    杨过：实际上就是创建了两个map,初始化map和单例map。
+    1、调用BeanDefinition的有参构造创建对象，将名称和BeanDefinition对象放入初始化map
+    2、先获取单例map的value，空值->获取初始化map的value
+    3、创建Bean 并实例化，放入单例map中
+	*/
 	@Test
 	public void testBeanFactory() throws Exception {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
@@ -18,5 +23,7 @@ public class BeanDefinitionAndBeanDefinitionRegistryTest {
 
 		HelloService helloService = (HelloService) beanFactory.getBean("helloService");
 		helloService.sayHello();
+
+
 	}
 }
